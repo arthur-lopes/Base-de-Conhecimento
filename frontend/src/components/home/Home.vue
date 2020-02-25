e26c4c6f13024f35c4624ce49ce3cf10846cb27b
arthur-lopes <59693644+arthur-lopes@users.noreply.github.com>
2020-02-24 22:16:58 -0300

    Adição de arquivos base do projeto

---
+++ frontend/src/components/home/Home.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="home">
+    <PageTitle icon="fa fa-home" main="Dashboard" sub="Base de Conhecimento" />
+    <div class="stats">
+      <Stat title="Categorias" :value="stat.categories" icon="fa fa-folder" color="#d54d50" />
+      <Stat title="Artigos" :value="stat.articles" icon="fa fa-file" color="#3bc480" />
+      <Stat title="Usuários" :value="stat.users" icon="fa fa-user" color="#3282cd" />
+    </div>
+  </div>
+</template>
+
+<script>
+import PageTitle from "@/components/template/PageTitle"
+import Stat from "./Stat"
+import axios from "axios"
+import { baseApiUrl } from "@/global"
+
+export default {
+  name: "Home",
+  components: { PageTitle, Stat },
+  data: function() {
+    return {
+      stat: {}
+    }
+  },
+  methods: {
+    getStats() {
+      axios.get(`${baseApiUrl}/stats`).then(res => this.stat = res.data)
+    }
+  },
+  mounted() {
+    this.getStats()
+  }
+}
+</script>
+
+<style>
+    .stat {
+        flex: 1;
+        display: flex;
+        border-radius: 8px;
+        margin-right: 20px;
+        margin-bottom: 20px;
+        background-color: #fff;
+        padding: 20px;
+        border: 1px solid rgba(0, 0, 0, 0.2);
+        box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
+    }
+
+    .stat-icon {
+        display: flex;
+        align-items: center;
+    }
+
+    .stat-icon i {
+        font-size: 5rem;
+    }
+
+    .stat-info {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+    }
+
+    .stat-title {
+        font-size: 1.2rem;
+    }
+
+    .stat-value {
+        font-size: 3rem;
+    }
+
+</style>
---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="footer">
+      <span>Copyright <strong><a href="http://2ois.com.br">Dois</a></strong> - © 2019 - 2020</span>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #ddd;
+        color: #333;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+
+    strong a {
+        text-decoration: none;
+        color: #333;
+    }
+</style>
